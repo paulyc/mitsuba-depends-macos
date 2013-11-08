36703a5d09efcfdd141d05a9da6ba624a7d3d029
Wenzel Jakob <wenzel@cs.cornell.edu>
2013-11-08 12:20:45 +0100

    updated COLLADA-DOM library

--- include/collada-dom/dom.h
+++ include/collada-dom/dom.h
@@ -1,24 +1,46 @@
 /*
-* Copyright 2006 Sony Computer Entertainment Inc.
-*
-* Licensed under the MIT Open Source License, for details please see license.txt or the website
-* http://www.opensource.org/licenses/mit-license.php
-*
-*/ 
+ * Copyright 2006 Sony Computer Entertainment Inc.
+ *
+ * Licensed under the MIT Open Source License, for details please see license.txt or the website
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ */
 #ifndef __DOM__
 #define __DOM__
 
 class DAE;
 class daeMetaElement;
 
-extern daeString COLLADA_VERSION;
-extern daeString COLLADA_NAMESPACE;
+#ifdef COLLADA_DOM_SUPPORT150
+namespace ColladaDOM150 {
+extern DLLSPEC daeString COLLADA_VERSION;
+extern DLLSPEC daeString COLLADA_NAMESPACE;
 
 // Register all types
-void registerDomTypes(DAE& dae);
+DLLSPEC void registerDomTypes(DAE& dae);
 
 // Register all elements
-daeMetaElement* registerDomElements(DAE& dae);
+DLLSPEC daeMetaElement* registerDomElements(DAE& dae);
 
+DLLSPEC daeInt colladaTypeCount();
+
+}
+#endif
+
+#ifdef COLLADA_DOM_SUPPORT141
+namespace ColladaDOM141 {
+extern DLLSPEC daeString COLLADA_VERSION;
+extern DLLSPEC daeString COLLADA_NAMESPACE;
+
+// Register all types
+DLLSPEC void registerDomTypes(DAE& dae);
+
+// Register all elements
+DLLSPEC daeMetaElement* registerDomElements(DAE& dae);
+
+DLLSPEC daeInt colladaTypeCount();
+
+}
+#endif
 
 #endif // __DOM_INTERFACE__
